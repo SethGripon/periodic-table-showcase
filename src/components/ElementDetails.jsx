f762e2fcae1a75ad17574605c829d7c4f3ad5386
SethGripon <setharvinpgripon@iskolarngbayan.pup.edu.ph>
2024-03-05 23:01:07 +0800

    added element package and fix details

--- src/components/ElementDetails.jsx
+++ src/components/ElementDetails.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
+import anotherElement from 'element-histories'
 
-const ElementDetails = ({ element}) => {
-
-  // console.log(element)
+const ElementDetails = ({ element }) => {
+  const imgUrl = anotherElement.image(element.name)
 
   return (
     <div className='h-full w-full font-dmSansVariable pb-10 flex justify-center'>
       <div className='h-full w-[1000px] bg-gradient-to-r from-[#d7e9db] via-[#e2c6ee] to-[#ced9ed] rounded-lg p-3'>
-        <div className='bg-black bg-opacity-25 h-full w-full rounded-lg'>
-
+        <div className='bg-black bg-opacity-25 h-full w-full grid grid-cols-2 grid-rows-1 rounded-md gap-[8px] p-[8px]'>
+          <img className="h-full w-full rounded-md" src={imgUrl} alt="" />
+          <div className='h-full w-full flex flex-col items-center'>
+            <div>
+              Hello World!
+            </div>
+          </div>
         </div>
       </div>
     </div>
